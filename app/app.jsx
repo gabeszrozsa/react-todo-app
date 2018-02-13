49ff5020b73f497c004419e746524d2c039a6dae
Gabesz Rózsa <gabesz.rozsa@gmail.com>
2018-02-13 22:25:45 +0100

    init

---
+++ app/app.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Route, Router, IndexRoute, hashHistory} from 'react-router';
+
+import TodoApp from 'TodoApp';
+
+// Load foundation
+$(document).foundation();
+
+// App css
+require('style!css!sass!applicationStyles')
+
+ReactDOM.render(
+  <div>
+    <TodoApp/>
+  </div>,
+  document.getElementById('app')
+);
---
+++ app/tests/app.test.jsx
@@ -0,0 +1,8 @@
+// var expect = require('expect');
+// import exp
+//
+// describe('App', () => {
+//   it('should properly run tests', () => {
+//     expect(1).toBe(1);
+//   });
+// });
