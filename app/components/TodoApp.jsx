e64ba45a48279db9cf389eda0c088fa674dcd476
Gabesz Rózsa <gabesz.rozsa@gmail.com>
2018-02-13 22:52:01 +0100

    upd

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 
 export default class TodoApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        {
+          id: 1,
+          text: 'Walk the dog'
+        },
+        {
+          id: 2,
+          text: 'Clean the yard'
+        }
+      ]
+    }
+  }
+
   render() {
     return (
       <div>
-        TodoApp
+        Csá Dude!
       </div>
     );
   }
