51dde05c24873191c74346a500cbbaf661120819
Gabesz Rózsa <gabesz.rozsa@gmail.com>
2018-02-14 19:49:44 +0100

    search todos component

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import TodoList from 'TodoList';
+import AddTodo from 'AddTodo';
+import TodoSearch from 'TodoSearch';
 
 export default class TodoApp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      showCompleted: false,
+      searchText: '',
       todos: [
         {
           id: 1,
@@ -23,14 +27,29 @@ export default class TodoApp extends React.Component {
           text: 'Play video games'
         }
       ]
-    }
+    };
+
+    this.handleSearch = this.handleSearch.bind(this);
+  }
+
+  handleAddTodo(text) {
+    alert(`new todo: ${text}`);
+  }
+
+  handleSearch(showCompleted, searchText) {
+    this.setState({
+      showCompleted,
+      searchText: searchText.toLowerCase()
+    });
   }
 
   render() {
     const { todos } = this.state;
     return (
       <div>
+        <TodoSearch onSearch={this.handleSearch}/>
         <TodoList todos={todos}/>
+        <AddTodo onAddTodo={this.handleAddTodo}/>
       </div>
     );
   }
